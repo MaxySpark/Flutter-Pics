a2c9619290fdf7b34ecda26a003fd748c70fcd0e
MaxySpark <me@maxyspark.me>
2019-09-10 23:09:11 +0530

    counter state

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Lets See Some Images!'),
-       ),
-       floatingActionButton: FloatingActionButton(
-         onPressed: () {
-           print('Hi There');
-         },
-         child: Icon(Icons.add_circle),
-       ),
-     )
-   );
+        home: Scaffold(
+          appBar: AppBar(
+              title: Text('Lets See Some Images!'),
+          ),
+          body: Text('$counter'),
+          floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  counter += 1;
+                });
+              },
+              child: Icon(Icons.add_circle),
+          ),
+        )
+      );
   }
 }
